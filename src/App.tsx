cdbaa3fcbf46a26c8f57863de9d7916372d5d9e1
Eduardo Pedroso <eduardopedroso1983@gmail.com>
2021-03-20 13:51:19 -0300

    first-commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Home from "./pages/Home"
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import { Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Switch>
+
+      <Route exact path="/">
+          <Home />
+        </Route>
+
+        <Route path="/signin">
+          <SignIn />
+        </Route>
+
+        <Route path="/signup">
+          <SignUp />
+        </Route>
+      </Switch>
     </div>
   );
 }
